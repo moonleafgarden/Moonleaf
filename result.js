9cd290d6b798d82f75889b47095c5d80436dc73a
madinabonus258-jpg <madinabonus258@gmail.com>
2026-07-17 20:23:48 +0500

    Update result.js

--- result.js
+++ result.js
@@ -1,48 +1,52 @@
-const score = localStorage.getItem("score");
+const score = Number(localStorage.getItem("score"));
+
+const scores = JSON.parse(localStorage.getItem("levelScores"));
 
 const levelText = document.getElementById("level");
 const scoreText = document.getElementById("score");
 const startButton = document.getElementById("start");
 
-scoreText.textContent = `Score: ${score}/30`;
+scoreText.textContent = `Score: ${score} / 30`;
 
-let level = "";
-let page = "";
+let level = "A1";
+let page = "a1.html";
 
-if(score <= 5){
+/* Highest completed level */
 
-    level = "🌱 A1";
-    page = "a1.html";
+if(scores.C2 >= 3){
 
-}
-else if(score <= 10){
-
-    level = "🍃 A2";
-    page = "a2.html";
+    level = "⭐ C2";
+    page = "c2.html";
 
 }
-else if(score <= 15){
-
-    level = "📖 B1";
-    page = "b1.html";
-
-}
-else if(score <= 20){
-
-    level = "🌿 B2";
-    page = "b2.html";
-
-}
-else if(score <= 25){
+else if(scores.C1 >= 3){
 
     level = "🌙 C1";
     page = "c1.html";
 
 }
+else if(scores.B2 >= 3){
+
+    level = "🌿 B2";
+    page = "b2.html";
+
+}
+else if(scores.B1 >= 3){
+
+    level = "📖 B1";
+    page = "b1.html";
+
+}
+else if(scores.A2 >= 3){
+
+    level = "🍃 A2";
+    page = "a2.html";
+
+}
 else{
 
-    level = "⭐ C2";
-    page = "c2.html";
+    level = "🌱 A1";
+    page = "a1.html";
 
 }
 
