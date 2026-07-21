ca7ab0ec9d7a9c957bf52c9791041b1a747bb613
madinabonus258-jpg <madinabonus258@gmail.com>
2026-07-21 11:34:46 +0500

    Update titles in chapter.js for clarity

--- chapter.js
+++ chapter.js
@@ -11,7 +11,7 @@ const chapters = [
     },
     {
         id: 3,
-        title: "Articles",
+        title: "Articles (a / an)",
         status: "locked"
     },
     {
@@ -21,7 +21,102 @@ const chapters = [
     },
     {
         id: 5,
-        title: "This / That / These / Those",
+        title: "This • That • These • Those",
+        status: "locked"
+    },
+    {
+        id: 6,
+        title: "Possessive Adjectives",
+        status: "locked"
+    },
+    {
+        id: 7,
+        title: "There is / There are",
+        status: "locked"
+    },
+    {
+        id: 8,
+        title: "Have got / Has got",
+        status: "locked"
+    },
+    {
+        id: 9,
+        title: "Present Simple",
+        status: "locked"
+    },
+    {
+        id: 10,
+        title: "Questions in Present Simple",
+        status: "locked"
+    },
+    {
+        id: 11,
+        title: "Short Answers",
+        status: "locked"
+    },
+    {
+        id: 12,
+        title: "Can / Can't",
+        status: "locked"
+    },
+    {
+        id: 13,
+        title: "Imperatives",
+        status: "locked"
+    },
+    {
+        id: 14,
+        title: "Prepositions of Place",
+        status: "locked"
+    },
+    {
+        id: 15,
+        title: "Prepositions of Time",
+        status: "locked"
+    },
+    {
+        id: 16,
+        title: "Present Continuous",
+        status: "locked"
+    },
+    {
+        id: 17,
+        title: "Like + -ing",
+        status: "locked"
+    },
+    {
+        id: 18,
+        title: "Object Pronouns",
+        status: "locked"
+    },
+    {
+        id: 19,
+        title: "Possessive 's",
+        status: "locked"
+    },
+    {
+        id: 20,
+        title: "Adverbs of Frequency",
+        status: "locked"
+    },
+    {
+        id: 21,
+        title: "Some / Any",
+        status: "locked"
+    },
+    {
+        id: 22,
+        title: "Countable & Uncountable",
+        status: "locked"
+    },
+    {
+        id: 23,
+        title: "Would Like",
+        status: "locked"
+    },
+    {
+        id: 24,
+        title: "Final Review",
         status: "locked"
     }
 ];
