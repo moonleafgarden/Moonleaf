1b7dc5fed5dda03b042f77905a9b0a5df6efefa7
madinabonus258-jpg <madinabonus258@gmail.com>
2026-07-21 11:13:05 +0500

    Create chapter.js

---
+++ chapter.js
@@ -0,0 +1,27 @@
+const chapters = [
+    {
+        id: 1,
+        title: "The Verb 'to be'",
+        status: "opened"
+    },
+    {
+        id: 2,
+        title: "Subject Pronouns",
+        status: "locked"
+    },
+    {
+        id: 3,
+        title: "Articles",
+        status: "locked"
+    },
+    {
+        id: 4,
+        title: "Plural Nouns",
+        status: "locked"
+    },
+    {
+        id: 5,
+        title: "This / That / These / Those",
+        status: "locked"
+    }
+];
