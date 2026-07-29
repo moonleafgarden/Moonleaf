83d0cfaa76f46a311cb672538d1069327ae99871
ms <madinabonus258@gmail.com>
2026-07-29 22:58:36 +0500

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,31 @@ function openBook(button){
     panel.classList.toggle("show");
 }
 
+
+function openLesson(id) {
+
+    const lesson = vocabularyLessons[id];
+
+    const container = document.getElementById("lesson-container");
+
+    if (!lesson) {
+        container.innerHTML = "<h2>Lesson not found</h2>";
+        return;
+    }
+
+    container.innerHTML = `
+        <div class="lesson-content show">
+            <h2>${lesson.title}</h2>
+
+            ${lesson.description}
+
+            <button onclick="finishChapter(${id})">
+                🌸 Finish Chapter
+            </button>
+        </div>
+    `;
+}
+
+function finishChapter(id){
+    alert("Chapter " + id + " completed!");
+}
